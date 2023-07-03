241d8ec741a916cb02f1726b22c87b0e59213dd9
Bahar <137733405+bahar-rze@users.noreply.github.com>
2023-07-04 02:16:54 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,252 +1,179 @@
 ﻿using System;
-namespace sodoko
+namespace Sodoku
 {
     class Project
     {
-        private static int SIZE = 9; 
-        private static int UNASSIGNED = 0; 
+        private static int SIZE = 9;
+        private static int UNASSIGNED = 0;
         private static int RANDOMFILLER = 2;
-        private static int USERFILLER = 1; 
+        private static int USERFILLER = 1;
 
         public static void Main()
         {
-            int[,] sudoku = new int[SIZE, SIZE];
-            SudokuSolver S = new SudokuSolver((short) UNASSIGNED, SIZE, RANDOMFILLER, UNASSIGNED); 
-            int p = 0;
+            SudokuSolver s = new SudokuSolver((short) UNASSIGNED, SIZE, RANDOMFILLER, UNASSIGNED);
             Console.WriteLine("Enter the option you want"
                               + "1-Fill sudoku yourself"
                               + "2-Fill sudoku random");
-            p = Convert.ToInt16(Console.ReadLine());
+            int p = 0;
             if (p == USERFILLER)
             {
-                S.sudokuFiller();
+                s.SudokuFiller();
             }
-            else {
-                int difficulty = 0;
-                Console.WriteLine("Enter Difficulty: (between 2-4 for quick result)");
-                difficulty = Convert.ToInt32(Console.ReadLine());
-                S.fillRandSudoku(difficulty);
+            else
+            {
+                Console.WriteLine();
             }
             Console.WriteLine("The sudoku :");
-            S.printCurrentSudoku();
-            S.solver();
+            s.PrintCurrentSudoku();
+            s.Solver();
+            Console.ReadKey();
         }
         class SudokuSolver
         {
-            private int unassigned;
-            private int size;
-            private int randomfiller;
-            private int userfiller;
-            private double speed;
-            private int[,] sudoku;
+            public int _random;
+            private int _unassigned;
+            private int _size;
+            private int _user;
+            private int[,] _sudoku;
 
             public int Unassigned
             {
-                get { return unassigned; }
-                set { unassigned = value; }
+                get { return _unassigned; }
+                set { _unassigned = value; }
             }
-
             public int Size
             {
-                get { return size; }
-                set { size = value; }
+                get { return _size;}
+                set { _size = value; }
             }
-
-            public int Randomfiller
+            public int User
             {
-                get { return randomfiller; }
-                set { randomfiller = value; }
+                get { return _user; }
+                set { _user = value; }
             }
-
-            public int Userfiller
+            public int Random
             {
-                get { return userfiller; }
-                set { userfiller = value; }
+                get { return _random; }
+                set
+                { _random = value; }
             }
-
             public int[,] Sudoku
             {
-                get { return sudoku; }
-                set { sudoku = value; }
+                get { return _sudoku; }
+                set { _sudoku = value; }
             }
 
-            public SudokuSolver(int unassigned, int size, int randomfiller, int userfiller, int[,] sudoku)
+            public SudokuSolver(int unassigned, int size, int random, int user, int[,] sudoku)
             {
-                ///تابع سازنده
+                Random = random;
                 Unassigned = unassigned;
                 Size = size;
-                Randomfiller = randomfiller;
-                Userfiller = userfiller;
+                User = user;
                 Sudoku = sudoku;
             }
-
-            public SudokuSolver()
+            public SudokuSolver(short unassigned, int size, int random, int user)
             {
-                ///تایع مخرب
+                Random = random;
+                _unassigned = unassigned;
+                _user = user;
             }
-
-            public SudokuSolver(short s, int i, int randomfiller1, int unassigned1)
+public SudokuSolver(){}
+            /// مخرب
+            public int[,] SudokuFiller()
             {
-                int p = 0;
-            }
+                int n = _unassigned;
+                string M;
 
-            public int[,] sudokuFiller()
-            {
-                int userInpInt = unassigned;
-                string userInpStr;
-                bool isExists = false;
-
-                for (int i = 0; i < size; i++)
+                for (int i = 0; i < _size; i++)
                 {
-                    for (int j = 0; j < size; j++)
+                    for (int j = 0; j < _size; j++)
                     {
                         Console.WriteLine("The current sudoku:");
-                        printCurrentSudoku();
                         {
                             Console.WriteLine("Enter el [" + i + "][" + j + "]:");
-                            userInpStr = Console.ReadLine();
+                            M = Console.ReadLine();
                             if (
-                                userInpStr == "" || userInpStr == " " ||
-                                userInpStr == null)
+                                M == "" || M == " ")
                             {
-                                userInpInt = unassigned; 
+                                n = _unassigned;
                             }
                             else
                             {
-                                userInpInt = Convert.ToInt16(userInpStr);
                             }
-
-                            if (userInpInt > 9 || userInpInt < 0)
+                            if (M != null && (n > 9 || n < 0))
                             {
                                 Console.WriteLine("ERR:enter number between 0-9(space or enter for skipping) !!!");
-                            }
-
-                            isExists = checkDuplicateNum(userInpInt, i, j);
-                            if (isExists && userInpInt != unassigned)
-                            {
-                                Console.WriteLine("dont repeat two num in row or col تو سطر یا ستون یه عدد یکسان هست");
-                            }
-                            else
-                            {
-                                isExists = false;
                             }
                         }
                     }
                 }
-
-                return sudoku;
+                return _sudoku;
             }
-            public void printCurrentSudoku()
+            public void PrintCurrentSudoku()
             {
-                for (int i = 0; i < size; i++)
+                for (int i = 0; i < _size; i++)
                 {
-                    for (int j = 0; j < size; j++)
+                    for (int j = 0; j < _size; j++)
                     {
-                        Console.Write(sudoku[i, j]);
-                        if (j != 0 || j != size)
+                        Console.Write(_sudoku[i, j]);
+                        if (j != 0 || j != _size)
                         {
                             Console.Write("|");
                         }
                     }
-
-                    Console.WriteLine("0 0 3 0 2 0 6 0 0 ");
-                    Console.WriteLine("9 0 0 3 0 5 0 0 1 ");
-                    Console.WriteLine("0 0 1 8 0 6 4 0 0 ");
-                    Console.WriteLine("0 0 8 1 0 2 9 0 0 ");
-                    Console.WriteLine("7 0 0 0 0 0 0 0 8 ");
-                    Console.WriteLine("0 0 6 7 0 8 2 0 0 ");
-                    Console.WriteLine("0 0 2 6 0 9 5 0 0 ");
-                    Console.WriteLine("8 0 0 2 0 3 0 0 9 ");
-                    Console.WriteLine("0 0 5 0 1 0 3 0 0 ");
                 }
             }
-            private bool checkDuplicateNum(int num, int x, int y)
+            private bool CheckDuplicateNum(int num, int x, int y)
             {
                 bool isExists = false;
-                for (int i = 0; i < size; i++)
-                { if (sudoku[i, y] == num)
+                for (int i = 0; i < _size; i++)
+                {
+                    if (_sudoku[i, y] == num)
                     {
                         isExists = true;
                     }
-                    if (sudoku[x, i] == num)
+                    if (_sudoku[x, i] == num)
                     {
                         isExists = true;
                     }
                 }
-
                 return isExists;
             }
-            private bool isFullSudoku()
-            {
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        if (sudoku[i, j] == unassigned)
-                        {
-                            return false;
-                        }
-                    }
-                }
-
-                return true;
-            }
-            public void fillRandSudoku(int difficulty)
-            {
-                int[] rands = new int[difficulty];
-                Random r = new Random();
-                Random r2 = new Random();
-                for (int i = 0; i < size; i++)
-                {
-                    //? Get random cell PLACE in one arr
-                    for (int k = 0; k < difficulty; k++)
-                    {
-                        rands[k] = r.Next(0, size);
-                    }
-
-                    for (int j = 0; j < size; j++)
-                    {
-                        if (((IList) rands).Contains(j))
-                        {
-                            for (int z = 0; z < size; z++)
-                            {
-                                if (!checkDuplicateNum(z, i, j))
-                                {
-                                    sudoku[i, j] = z;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            public bool solver()
+            public bool Solver()
             {
                 Console.Clear();
-                printCurrentSudoku();
-                System.Threading.Thread.Sleep(
-                    (int) System.TimeSpan.FromSeconds(speed).TotalMilliseconds);
-                Console.WriteLine("----------------------");
-                for (int i = 0; i < size; i++)
+                PrintCurrentSudoku();
+                Console.WriteLine("0 0 3 0 2 0 6 0 0 ");
+                Console.WriteLine("9 0 0 3 0 5 0 0 1 ");
+                Console.WriteLine("0 0 1 8 0 6 4 0 0 ");
+                Console.WriteLine("0 0 8 1 0 2 9 0 0 ");
+                Console.WriteLine("7 0 0 0 0 0 0 0 8 ");
+                Console.WriteLine("0 0 6 7 0 8 2 0 0 ");
+                Console.WriteLine("0 0 2 6 0 9 5 0 0 ");
+                Console.WriteLine("8 0 0 2 0 3 0 0 9 ");
+                Console.WriteLine("0 0 5 0 1 0 3 0 0 ");
+                for (int i = 0; i < _size; i++)
                 {
-                    for (int j = 0; j < size; j++)
+                    for (int j = 0; j < _size; j++)
                     {
-                        if (sudoku[i, j] == unassigned)
+                        if (_sudoku[i, j] == _unassigned)
                         {
-                            for (int n = 1; n <= size; n++)
+                            for (int n = 1; n <= _size; n++)
                             {
-                                if (!checkDuplicateNum(n, i, j))
+                                if (!CheckDuplicateNum(n, i, j))
                                 {
-                                    sudoku[i, j] = n;
-                                    if (solver())
+                                    _sudoku[i, j] = n;
+                                    if (Solver())
                                     {
                                         return true;
                                     }
                                     else
                                     {
-                                        sudoku[i, j] = unassigned;
+                                        _sudoku[i, j] = _unassigned;
                                     }
                                 }
                             }
+
                             return false;
                         }
                     }
@@ -256,4 +183,5 @@ namespace sodoko
         }
     }
 }
+
  
